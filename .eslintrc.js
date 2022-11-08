e69b8f547a58281938ed0a876886e6f345ad2b8c
Alexandr Rylkov <skynerstudent@gmail.com>
2022-11-08 23:34:33 +0300

    init: start project songbird

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'linebreak-style': 0,
+  },
+  globals: {
+    window: true,
+    document: true,
+    localStorage: true,
+    getComputedStyle: true,
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/components/app.js',
+  devtool: 'eval',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  mode: 'development',
+};
